312445cbe45156f04edacedcf154bcfd77aaceef
Jindaporn Sookying <jindaporn.sy@gmail.com>
2025-03-28 11:33:48 +0700

    add search state

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import AppRoutes from "./routes/Routes";
 import "./index.css";
+import SearchProvider from "./context/SearchProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppRoutes />
+      <SearchProvider>
+        <AppRoutes />
+      </SearchProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
