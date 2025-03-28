4961294859ab44a836b74669a0fd901ba840b9cc
Jindaporn Sookying <jindaporn.sy@gmail.com>
2025-03-28 15:33:02 +0700

    add post count

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -40,6 +40,8 @@ const HomePage = () => {
   const circleRef = useRef<google.maps.Circle | null>(null);
   const mapRef = useRef<google.maps.Map | null>(null);
   const [posts, setPosts] = useState<Post[]>([]);
+  const [counts, setCounts] = useState({ lost: 0, found: 0, adoption: 0 });
+
   const handleClick = () => {
     navigate("/lost-cat");
   };
@@ -67,6 +69,11 @@ const HomePage = () => {
         ]);
 
         setPosts([...lost, ...found, ...adoption]);
+        setCounts({
+          lost: lost.filter((p: Post) => p.status === "active").length,
+          found: found.filter((p: Post) => p.status === "active").length,
+          adoption: adoption.filter((p: Post) => p.status === "active").length,
+        });
       } catch (error) {
         console.error("Failed to fetch posts", error);
       }
@@ -122,6 +129,7 @@ const HomePage = () => {
       );
     }
   }, [mapLoaded]);
+
   useEffect(() => {
     if (location) {
       updateCircle(location);
@@ -129,7 +137,7 @@ const HomePage = () => {
   }, [radius]);
 
   return (
-    <div className="w-full h-fit px-4 py-10">
+    <div className="w-full h-fit px-4 pb-10">
       <div className="h-screen flex flex-col-reverse lg:flex-row items-center gap-10 lg:gap-20 max-w-6xl mx-auto -mt-32">
         {/* Left Section: Text and Button */}
         <div className="text-center lg:text-left">
@@ -164,68 +172,101 @@ const HomePage = () => {
         </div>
       </div>
 
-      {/* 🗺️ Map Section Title - add spacing above */}
-      <div className="mt-10 text-center text-xl font-semibold text-gray-700 mb-4">
-        <h1 className="text-3xl">สำรวจตำแหน่งแมวในพื้นที่ของคุณ</h1>
-        <h2>แมวหาย / แมวหาเจ้าของ / แมวหาบ้าน</h2>
+      {/* Counts Section */}
+      <div className="text-sm text-gray-600 mt-6 mb-4 w-full bg-white-300 px-5 py-8 rounded-xl border-2 border-orange-400">
+        <div className="grid grid-cols-4 gap-4 text-center">
+          {/* Title */}
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-xl font-medium text-gray-700 col-span-1">
+              จำนวนแมวที่รอความช่วยเหลือ
+            </p>
+          </div>
+
+          {/* แมวหาย */}
+          <div>
+            <p className="text-7xl font-bold text-red-500">{counts.lost}</p>
+            <p className="mt-1 text-xl">แมวหาย</p>
+          </div>
+
+          {/* แมวหาเจ้าของ */}
+          <div>
+            <p className="text-7xl font-bold text-blue-500">{counts.found}</p>
+            <p className="mt-1 text-xl">แมวหาเจ้าของ</p>
+          </div>
+
+          {/* แมวหาบ้าน */}
+          <div>
+            <p className="text-7xl font-bold text-green-500">
+              {counts.adoption}
+            </p>
+            <p className="mt-1 text-xl">แมวหาบ้าน</p>
+          </div>
+        </div>
       </div>
 
-      <div className="mt-10 mb-10 w-full max-w-6xl mx-auto h-[600px]">
-        <GoogleMap
-          onLoad={handleMapLoad}
-          mapContainerStyle={{ width: "100%", height: "100%" }}
-          center={mapCenter}
-          zoom={15}
-          onClick={(e) => {
-            const newLocation = {
-              lat: e.latLng?.lat() ?? 0,
-              lng: e.latLng?.lng() ?? 0,
-            };
-            setLocation(newLocation);
-            updateCircle(newLocation);
-          }}
-          options={{
-            disableDefaultUI: true,
-            zoomControl: true,
-            streetViewControl: false,
-          }}
-        >
-          {posts.map((post) => {
-            let borderColor = "border-gray-400";
-            if (post.post_type === "lost") borderColor = "border-red-500";
-            else if (post.post_type === "found")
-              borderColor = "border-blue-500";
-            else if (post.post_type === "adoption")
-              borderColor = "border-green-500";
+      {/* Map Section */}
+      <div className="mt-20 mb-10 py-2">
+        <div className="text-center text-xl font-semibold text-gray-700">
+          <h1 className="text-3xl">สำรวจตำแหน่งแมวในพื้นที่ของคุณ</h1>
+          <h2>แมวหาย / แมวหาเจ้าของ / แมวหาบ้าน</h2>
+        </div>
 
-            return (
-              <OverlayView
-                key={post.post_id}
-                position={{
-                  lat: Number(post.location.latitude),
-                  lng: Number(post.location.longitude),
-                }}
-                mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
-              >
-                <div
-                  onClick={() => navigate(`/cat-detail/${post.post_id}`)}
-                  className={`w-12 h-12 rounded-full ${borderColor} border-4 overflow-hidden cursor-pointer shadow-md hover:scale-110 transition-transform duration-200`}
-                  title={`${post.cat_name || "ไม่ระบุ"} (${post.post_type})`}
+        <div className="mt-5 w-full max-w-6xl mx-auto h-[600px]">
+          <GoogleMap
+            onLoad={handleMapLoad}
+            mapContainerStyle={{ width: "100%", height: "100%" }}
+            center={mapCenter}
+            zoom={15}
+            onClick={(e) => {
+              const newLocation = {
+                lat: e.latLng?.lat() ?? 0,
+                lng: e.latLng?.lng() ?? 0,
+              };
+              setLocation(newLocation);
+              updateCircle(newLocation);
+            }}
+            options={{
+              disableDefaultUI: true,
+              zoomControl: true,
+              streetViewControl: false,
+            }}
+          >
+            {posts.map((post) => {
+              let borderColor = "border-gray-400";
+              if (post.post_type === "lost") borderColor = "border-red-500";
+              else if (post.post_type === "found")
+                borderColor = "border-blue-500";
+              else if (post.post_type === "adoption")
+                borderColor = "border-green-500";
+
+              return (
+                <OverlayView
+                  key={post.post_id}
+                  position={{
+                    lat: Number(post.location.latitude),
+                    lng: Number(post.location.longitude),
+                  }}
+                  mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
                 >
-                  <img
-                    src={post.cat_image?.image_path}
-                    alt={post.cat_name || "cat"}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-              </OverlayView>
-            );
-          })}
-        </GoogleMap>
+                  <div
+                    onClick={() => navigate(`/cat-detail/${post.post_id}`)}
+                    className={`w-12 h-12 rounded-full ${borderColor} border-4 overflow-hidden cursor-pointer shadow-md hover:scale-110 transition-transform duration-200`}
+                    title={`${post.cat_name || "ไม่ระบุ"} (${post.post_type})`}
+                  >
+                    <img
+                      src={post.cat_image?.image_path}
+                      alt={post.cat_name || "cat"}
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                </OverlayView>
+              );
+            })}
+          </GoogleMap>
+        </div>
       </div>
     </div>
   );
 };
 
 export default HomePage;
-
